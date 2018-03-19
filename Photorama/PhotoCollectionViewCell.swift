7dbca9b245027cd3f499ffef62af51d30d8abf5d
SergioBrunacci <sergio.a.brunacci@gmail.com>
2018-03-19 19:57:43 -0400

    Creation of the PhotoCollectionViewCell.swift

---
+++ Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Sergio de Almeida Brunacci on 2018-03-19.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+}
